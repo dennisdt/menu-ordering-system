ddf2b89d58b03e5563a1feb41a43f2e50f98d834
Dennis Tran <dennistran714@gmail.com>
2018-07-07 13:23:18 -0700

    fixed dashboard and prepared for heroku deployment

--- app.py
+++ app.py
@@ -1,25 +1,15 @@
+import numpy as np
 import pandas as pd
 import pymongo
 from datetime import datetime, timedelta
 from calendar import monthrange
-import plotly.plotly as py
-import plotly.graph_objs as go
-import sys, json
-import os.path
+import os
 
-from flask import Flask, jsonify, render_template, send_from_directory, session
+from flask import Flask, jsonify, render_template, send_from_directory, request, redirect
 app = Flask(__name__)
 
-# time today
-time_now = datetime.now()
-# beginning of the day
-time_day = datetime.combine(time_now.date(), datetime.min.time())
-# beginning of the year
-time_year = datetime.combine(datetime.min.date(), datetime.min.time())
-
-# DB credentials
-dbuser = "dennisdt"
-dbpassword = "testing123"
+dbuser = os.environ.get('dbuser')
+dbpassword = os.environ.get('dbpassword')
 
 # connecting to mongoDB
 conn = f"mongodb://{dbuser}:{dbpassword}@ds123981.mlab.com:23981/overclocked"
@@ -35,10 +25,24 @@ menu_df = pd.DataFrame(list(query))
 def index():
     return render_template('index.html')
 
-@app.route('/get_charts/')
-@app.route('/get_charts/<kind>/<interval>')
+@app.route('/send', methods=["POST"])
+def send():
+    start_date = request.form["startDate"]
+    end_date = request.form["endDate"]
+    kind = request.form["kind"]
+    interval = request.form["interval"]
+    button = request.form["button"]
+
+    if button == 'sales':
+        print("helloworld")
+        return redirect(f'/get_perf_report/{start_date}/{end_date}/{interval}')
+    elif button == 'items':
+        print("another route")
+        return redirect(f'/get_item_report/{start_date}/{end_date}/{kind}/{interval}')
+
+
 @app.route('/get_charts/<start_date>/<end_date>/<kind>/<interval>')
-def get_charts(start_date=time_day, end_date=time_now, kind="Revenue", interval='Hour'):
+def get_charts(start_date, end_date, kind="Revenue", interval='Hour'):
     # function to build charts
     # defaults to daily data if no time argument is provided
     # start_time and end_time must be in datetime format
@@ -89,26 +93,6 @@ def get_charts(start_date=time_day, end_date=time_now, kind="Revenue", interval=
         # yearly groupby
         grouped = merged_df.groupby([times.year]).sum()
         label = "Year"
-        
-    # to be moved or modified
-    '''
-    if (end_date - start_date) <= (end_date - (datetime.now() - timedelta(days=7))):
-        # daily groupby
-        grouped = merged_df.groupby([times.hour]).sum()
-        label = "Hour"
-    elif (end_date - start_date) <= (end_date - (datetime.now() - timedelta(days=31))):
-        # weekly groupby
-        grouped = merged_df.groupby([times.day]).sum()
-        label = "Day"
-    elif (end_date - start_date) <= (end_date - (datetime.now() - timedelta(days=365))):
-        # monthly groupby
-        grouped = merged_df.groupby([times.month]).sum()
-        label = "Month"
-    else:
-        # yearly groupby
-        grouped = merged_df.groupby([times.year]).sum()
-        label = "Year"
-    '''
     
     if kind == 'Quantity':
         # x and y values for quantity
@@ -119,47 +103,16 @@ def get_charts(start_date=time_day, end_date=time_now, kind="Revenue", interval=
         # x and y values for revenue
         x_val=grouped["total"].index.tolist()
         y_val=grouped["total"].tolist()
-        
-    '''    
-    # output figure
-    fig = dict(
-        data = [go.Bar(
-                x=x_val,
-                y=y_val,
-                marker=dict(
-                    color='rgb(158,202,225)',
-                    line=dict(
-                        color='rgb(8,48,107)',
-                        width=1.5),
-                ),
-                opacity=0.6
-            )],
 
-        layout = {
-          "hovermode": "closest", 
-          "margin": {
-            "r": 10, 
-            "t": 25, 
-            "b": 40, 
-            "l": 60
-          }, 
-          "title": f"{kind} vs {label}", 
-          "xaxis": {
-            "title": f"{label}"
-          }, 
-          "yaxis": {
-            "title": f"{kind}"
-          }
-        }
-    )
-    '''
-    data = {"x": list(map(int, x_val)), "y": y_val, "kind": kind, "label": label}
+    if type(x_val[0]) is np.int64:
+        data = {"x": list(map(int, x_val)), "y": y_val, "kind": kind, "label": label}
+    else:
+        data = {"x": [x.strftime('%m-%d-%Y') for x in x_val], "y": y_val, "kind": kind, "label": label}
+
     return jsonify(data)
 
-@app.route('/get_top_items/')
-@app.route('/get_top_items/<kind>/<interval>')
 @app.route('/get_top_items/<start_date>/<end_date>/<kind>/<interval>')
-def get_top_items(start_date=(time_now - timedelta(days=7)), end_date=time_now, kind="Revenue", interval="Hour"):
+def get_top_items(start_date, end_date, kind="Revenue", interval="Hour"):
 
     # converts start_date and end_date to datetime objects
     if (type(start_date) is not datetime) and (type(end_date) is not datetime):
@@ -201,48 +154,12 @@ def get_top_items(start_date=(time_now - timedelta(days=7)), end_date=time_now, 
         top_item_df.sort_values(by=['itemQTY'], ascending=False, inplace=True)
         x_val=top_item_df["title"].tolist()
         y_val=top_item_df["itemQTY"].tolist()
-        
-    '''
-    # item vs revenue
-    fig = dict(
-        data = [go.Bar(
-                x=x_val,
-                y=y_val,
-                text=list(top_item_df["itemID"]),
-                marker=dict(
-                    color='rgb(158,202,225)',
-                    line=dict(
-                        color='rgb(8,48,107)',
-                        width=1.5),
-                ),
-                opacity=0.6
-            )],
-
-        layout = {
-          "hovermode": "closest", 
-          "margin": {
-            "r": 10, 
-            "t": 25, 
-            "b": 60, 
-            "l": 60
-          }, 
-          "title": f"{kind} vs Items", 
-          "xaxis": {
-            "domain": [0, 1], 
-            "title": "Item"
-          }, 
-          "yaxis": {
-            "domain": [0, 1], 
-            "title": f"{kind}"
-          }
-        }
-    )
-    '''
+    
     data = {"x": x_val, "y": y_val, "kind": kind, "itemID": list(map(int, top_item_df["itemID"].tolist()))}
     return jsonify(data)
-@app.route('/get_perf_report/')
+
 @app.route('/get_perf_report/<start_date>/<end_date>/<interval>')
-def get_perf_report(start_date=time_day, end_date=time_now, interval='Hour'):
+def get_perf_report(start_date, end_date, interval='Hour'):
     # converts start_date and end_date to datetime objects
     if (type(start_date) is not datetime) and (type(end_date) is not datetime):
         start_date = datetime.strptime(start_date, '%Y-%m-%d')
@@ -305,7 +222,6 @@ def get_perf_report(start_date=time_day, end_date=time_now, interval='Hour'):
         # output link to download
         return send_from_directory('reports/performance/', f'{start_date.date()}_{end_date.date()}_{interval}_report.xlsx', as_attachment=True)
 
-@app.route('/get_item_report/')
 @app.route('/get_item_report/<start_date>/<end_date>/<kind>/<interval>')
 def get_item_report(start_date=time_year, end_date=time_now, kind='Revenue', interval='Hour'):
     # converts start_date and end_date to datetime objects
--- client/src/actions/index.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import { FETCH_USER } from './types';
-
-export const fetchUser = () => {
-  return async function(dispatch) {
-    const res = await axios.get('/api/user_info')
-    dispatch({ type: FETCH_USER, payload: res.data });
-  };
-};
